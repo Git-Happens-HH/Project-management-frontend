38ab50aa2dcd5378acdb71ad23bfaa59b24f547b
Supr0en <erik.ruotsalainen1@gmail.com>
2026-02-24 17:05:19 +0200

    added simple routing to projects page from welcome page

--- prokress/src/App.tsx
+++ prokress/src/App.tsx
@@ -3,12 +3,16 @@ import WelcomePage from './pages/WelcomePage.tsx'
 import ProjectsPage from './pages/ProjectsPage.tsx'
 import AppBar from './Components/AppBar.tsx'
 import Footer from './Components/Footer.tsx'
+import { Routes, Route } from 'react-router-dom'
 function App() {
 
   return(
     <>
-      <AppBar /> 
-      <ProjectsPage />
+      <AppBar />
+      <Routes>
+         <Route path="/" element={<WelcomePage />} />
+         <Route path="/projects" element={<ProjectsPage />} />
+      </Routes>
       <Footer />
     </> 
   )
