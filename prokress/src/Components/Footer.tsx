28003f5e414b2589d61e2a62ab374a899e58055f
Supr0en <erik.ruotsalainen1@gmail.com>
2026-02-25 18:38:55 +0200

    Mass refactoring of class to className for builds to success plus configuration for azure

--- prokress/src/Components/Footer.tsx
+++ prokress/src/Components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return(
-    <div class="flex flex-col justify-center items-center p-10">
+    <div className="flex flex-col justify-center items-center p-10">
       <p>©2026 GitHappensHH</p>
     </div>
   )  
--- prokress/src/pages/ProjectsPage.tsx
+++ prokress/src/pages/ProjectsPage.tsx
@@ -1,7 +1,7 @@
 import ProjectCard from '../Components/ProjectCard.tsx'
 import { useState } from 'react'
 type projectType = {
-   projectId: number;
+   projectId: string;
    title: string,
    description: string,
    createdAt: string,
@@ -11,10 +11,10 @@ function ProjectsPage() {
    const [sharedProjects, setSharedProjects] = useState<projectType[]>([]);
    const curDate = new Intl.DateTimeFormat('en-GB').format(new Date());
    return (
-      <div class="flex flex-col min-h-screen items-center bg-(--prokress-beige-100)">
-         <div class="w-320 rounded-3xl mt-[5%] h-80 bg-(--prokress-beige-50) shadow-md">
-            <div class="bg-(--prokress-beige-0) rounded-t-3xl pt-2 pl-4 pb-2"><p class="text-(--prokress-black-700) text-2xl font-bold">Omat projektit</p></div>
-            <div class="flex flex-row gap-x-2 ml-4 mr-4 overflow-auto">
+      <div className="flex flex-col min-h-screen items-center bg-(--prokress-beige-100)">
+         <div className="w-320 rounded-3xl mt-[5%] h-80 bg-(--prokress-beige-50) shadow-md">
+            <div className="bg-(--prokress-beige-0) rounded-t-3xl pt-2 pl-4 pb-2"><p className="text-(--prokress-black-700) text-2xl font-bold">Omat projektit</p></div>
+            <div className="flex flex-row gap-x-2 ml-4 mr-4 overflow-auto">
                { myProjects.map((project, index) => {
                   return (
                      <ProjectCard project={project} index={index} />
@@ -23,14 +23,14 @@ function ProjectsPage() {
                <div onClick={() => {setMyProjects([
                   ...myProjects, {projectId:"2", title:"New project", description:"This is new project", createdAt:curDate}
                ]);}} 
-                  class="min-w-80 h-55 rounded-lg mt-4 m-2 bg-(--prokress-beige-0) text-(--prokress-black-700) shadow-md flex justify-center items-center">
-                  <div class="text-white font-bold py-2 px-2 rounded-full w-10 h-10 bg-(--prokress-black-500)"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg></div>
+                  className="min-w-80 h-55 rounded-lg mt-4 m-2 bg-(--prokress-beige-0) text-(--prokress-black-700) shadow-md flex justify-center items-center">
+                  <div className="text-white font-bold py-2 px-2 rounded-full w-10 h-10 bg-(--prokress-black-500)"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg></div>
                </div>
             </div>
          </div>
-         <div class="w-320 rounded-3xl mt-[5%] h-80 bg-(--prokress-beige-50) shadow-md">
-            <div class="bg-(--prokress-beige-0) rounded-t-3xl pt-2 pl-4 pb-2"><p class="text-(--prokress-black-700) text-2xl font-bold">Jaetut projektit</p></div>
-            <div class="flex flex-row gap-x-2 ml-4 mr-4 overflow-auto">
+         <div className="w-320 rounded-3xl mt-[5%] h-80 bg-(--prokress-beige-50) shadow-md">
+            <div className="bg-(--prokress-beige-0) rounded-t-3xl pt-2 pl-4 pb-2"><p className="text-(--prokress-black-700) text-2xl font-bold">Jaetut projektit</p></div>
+            <div className="flex flex-row gap-x-2 ml-4 mr-4 overflow-auto">
                { sharedProjects.map((project, index) => {
                   return (
                      <ProjectCard project={project} index={index}/>
@@ -40,8 +40,8 @@ function ProjectsPage() {
                onClick={() => {setSharedProjects([
                   ...sharedProjects, {projectId:"1", title:"New shared project", description:"New description", createdAt:curDate.toString()}
                ]);}} 
-                  class="min-w-80 h-55 rounded-lg mt-4 m-2 bg-(--prokress-beige-0) text-(--prokress-black-700) shadow-md flex justify-center items-center">
-                  <div class="text-white font-bold py-2 px-2 rounded-full w-10 h-10 bg-(--prokress-black-500)"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg></div>
+                  className="min-w-80 h-55 rounded-lg mt-4 m-2 bg-(--prokress-beige-0) text-(--prokress-black-700) shadow-md flex justify-center items-center">
+                  <div className="text-white font-bold py-2 px-2 rounded-full w-10 h-10 bg-(--prokress-black-500)"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg></div>
                </div>
             </div>
          </div>
