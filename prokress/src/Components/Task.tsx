abb69ce4da205ea4b7c3f8ce8bc458cc457173f2
Supr0en <erik.ruotsalainen1@gmail.com>
2026-03-22 12:46:18 +0200

    Project page with dnd + changes router to browserrouter from hashrouting

---
+++ prokress/src/Components/Task.tsx
@@ -0,0 +1,25 @@
+import {useState} from 'react'
+import {useSortable} from '@dnd-kit/react/sortable'
+
+function Task({ id, index, column }) {
+   const [isCollapsed, setIsCollapsed] = useState(parseInt(index) >= 4);
+   const toggleCollapse = () => {
+      setIsCollapsed(!isCollapsed);
+   };
+   const { ref, isDragging } = useSortable({ id, index, type: 'item', accept: 'item', group: column });
+   return (
+      <div ref={ ref } data-dragging={isDragging} className={`flex flex-col rounded w-[330px] border-solid border-black border-2 text-(--prokress-black-700) my-2 bg-(--prokress-beige-0) transition-all duration-300 ease-in-out ${isCollapsed ? 'h-10 overflow-hidden p-2' : 'h-32 p-2'}`} id={index}>
+         <div className='flex pb-1'>
+            <p className="w-9/10 text-md">{id}</p>
+            <button className="rounded-full w-0.8/10 hover:bg-gray-100 p-0.2" onClick={toggleCollapse}>
+            {isCollapsed ? 
+               <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#00000"><path d="M200-200v-240h80v160h160v80H200Zm480-320v-160H520v-80h240v240h-80Z"/></svg> : 
+               <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#00000"><path d="M440-440v240h-80v-160H200v-80h240Zm160-320v160h160v80H520v-240h80Z"/></svg>}
+            </button>
+         </div>
+         <div className={`border-t-1 pt-1 transition-all duration-300 ease-in-out ${isCollapsed ? 'hidden' : 'block'}`}><p>Description here</p></div>
+      </div>
+   )
+}
+
+export default Task
---
+++ prokress/src/Components/TaskList.tsx
@@ -0,0 +1,22 @@
+import Task from './Task.tsx'
+import {useDroppable} from '@dnd-kit/react';
+import {CollisionPriority} from '@dnd-kit/abstract';
+
+function TaskList ({children, id}) {
+   const {isDropTarget, ref} = useDroppable({
+    id,
+    type: 'column',
+    accept: 'item',
+    collisionPriority: CollisionPriority.Low,
+   });
+   const style = isDropTarget ? {background: '#00000030'} : undefined;
+
+   return (
+      <div ref={ref} style={style} className="border-solid rounded-xl min-w-[350px] h-[75vh] p-2 bg-(--prokress-beige-0) shadow overflow-auto"> 
+         <h1 className="text-(--prokress-black-700) text-lg font-bold">{id}</h1>   
+         {children}
+      </div>
+   )
+}
+
+export default TaskList
