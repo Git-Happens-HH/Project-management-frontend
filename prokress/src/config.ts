a3d2b419edd3963646675fa3e274397f4c553b83
Ruubern <rooperouhis@gmail.com>
2026-08-10 16:17:50 +0300

    refactor: change API_URL in config.ts to Production url

--- prokress/src/config.ts
+++ prokress/src/config.ts
@@ -2,5 +2,4 @@
 // export const API_URL = "http://localhost:8080";
 
 // Production
-// export const API_URL =
-"https://project-management-app-prokress-backend.2.rahtiapp.fi";
+export const API_URL = "https://project-management-app-prokress-backend.2.rahtiapp.fi";
